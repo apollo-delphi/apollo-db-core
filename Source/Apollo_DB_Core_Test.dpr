112b5bcd56055009b20d9e33e56096f1961d0f28
apollo.delphi.lib@gmail.com <apollo.delphi.lib@gmail.com>
2021-02-21 12:36:00 +0300

    dev

---
+++ Source/Apollo_DB_Core_Test.dpr
@@ -0,0 +1,19 @@
+program Apollo_DB_Core_Test;
+
+{$STRONGLINKTYPES ON}
+uses
+  Vcl.Forms,
+  System.SysUtils,
+  DUnitX.Loggers.GUI.VCL,
+  DUnitX.Loggers.Xml.NUnit,
+  DUnitX.TestFramework,
+  tstApollo_DB_Core in 'tstApollo_DB_Core.pas',
+  Apollo_DB_Core in 'Apollo_DB_Core.pas',
+  Apollo_Helpers in '..\Vendors\Apollo_Helpers\Apollo_Helpers.pas';
+
+begin
+  Application.Initialize;
+  Application.Title := 'DUnitX';
+  Application.CreateForm(TGUIVCLTestRunner, GUIVCLTestRunner);
+  Application.Run;
+end.
